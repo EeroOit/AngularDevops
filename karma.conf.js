b0195c1b88be9a54e5f34c751a0ee839babe61a2
eerooit <eero.oittinen@gmail.com>
2021-02-25 15:43:09 +0200

    karma addded

--- karma.conf.js
+++ karma.conf.js
@@ -39,6 +39,10 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    ChromeHeadlessCustom: {
+      base: 'ChromeHeadless',
+      flags: ['--no-sandbox', '--disable-gpu']
+    }
   });
 };
